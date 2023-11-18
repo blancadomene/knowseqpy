5f7bf0357384499956badfa06c2a351026b710d1
blancadomene <blancadomene@gmail.com>
2023-11-18 15:50:56 +0100

    Implemented and tested rna_seq_qa

--- knowseq/rna_seq_qa.py
+++ knowseq/rna_seq_qa.py
@@ -1,84 +1,111 @@
 import os
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from scipy.spatial.distance import pdist, squareform
-from scipy.stats import ks_2samp, zscore
-from statsmodels.robust.scale import mad
+from scipy.stats import kstest, median_abs_deviation
 
 
-def RNAseqQA(expressionMatrix, outdir="SamplesQualityAnalysis", toPNG=True, toPDF=True, toRemoval=False):
-    if not isinstance(expressionMatrix, pd.DataFrame):
-        raise ValueError("The class of expressionMatrix parameter must be a DataFrame.")
-    if not isinstance(toPNG, bool):
-        raise ValueError("toPNG parameter can only take the values True or False.")
-    if not isinstance(toPDF, bool):
-        raise ValueError("toPDF parameter can only take the values True or False.")
+def rna_seq_qa(expression_df: pd.DataFrame, output_dir: str = "SamplesQualityAnalysis") -> [pd.DataFrame, list]:
+    """
+    Perform the quality analysis of an expression matrix.
 
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+    TODO: This function in R generates different plots over expression data in order.
+          We want to return as a stats dict (min, max, etc) in case we want to plot using a separate func
 
-    print("Performing samples quality analysis...")
+    Parameters:
+    expression_df: A DataFrame that contains the gene expression values.
+    output_dir: The output directory to store the analysis results.
 
-    expressionMatrix = expressionMatrix.loc[~expressionMatrix.index.duplicated(keep='first')]
-    outliers = {}
-    found_outliers = [-1]
-    removed_outliers = []
+    Returns:
+    dict: A dictionary containing found outliers for each realized test or corrected data if to_removal is True.
+    """
+    # TODO: Check if there are NA values (manually removed them from the golden while testing)
+    # TODO: Improve performance
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    print("Running Distances Outliers Detection tests...")
-    distance_matrix = pd.DataFrame(squareform(pdist(expressionMatrix.T, 'cityblock')) / len(expressionMatrix),
-                                   columns=expressionMatrix.columns, index=expressionMatrix.columns)
-    distance_sum = distance_matrix.sum()
-    dist_data = pd.DataFrame({'x': distance_sum, 'y': distance_matrix.columns})
+    # Transpose the matrix to have samples as rows and genes as columns
+    # `pdist` function expects each sample to be in the rows of the input matrix, not the columns.
+    expression_df = expression_df.transpose()
 
-    if toPNG:
-        sns.heatmap(distance_matrix)
-        plt.title('Distances between arrays')
-        plt.savefig(os.path.join(outdir, 'distance-plot.png'), dpi=300)
-        plt.close()
+    outliers = {'Distance': [], 'KS': [], 'MAD': []}
 
-    q3 = dist_data['x'].quantile(0.75)
-    dist_limit = q3 + 1.5 * (dist_data['x'].quantile(0.75) - dist_data['x'].quantile(0.25))
+    # --- SAMPLES DISTANCES OUTLIERS DETECTION --- #
+    # Calculate the Manhattan distances between samples
+    manhattan_distances = pdist(expression_df.values, metric='cityblock')
+    manhattan_distance_matrix = squareform(manhattan_distances) / expression_df.shape[1]
+    distance_sum = np.sum(manhattan_distance_matrix, axis=0)
 
-    outliers['Distance'] = {'limit': dist_limit, 'outliers': distance_sum[distance_sum > dist_limit]}
+    # TODO: only upper threshold for some reason?
 
-    print("Done!")
+    # Calculate the threshold for outliers (Q3 + 1.5 * IQR)
+    q3 = np.percentile(distance_sum, 75)
+    iqr = np.subtract(*np.percentile(distance_sum, [75, 25]))
+    upper_distance_outlier_threshold = q3 + 1.5 * iqr
 
-    print("Running Kolmogorov-Smirnov tests...")
-    ks = expressionMatrix.apply(lambda x: ks_2samp(x, expressionMatrix.values.flatten())[0], axis=1)
+    # Detect distance-based outliers
+    distance_outliers = expression_df.index[distance_sum > upper_distance_outlier_threshold].tolist()
+    outliers['Distance'] = {'limit': upper_distance_outlier_threshold, 'outliers': distance_outliers}
 
-    ks_data = pd.DataFrame({'x': ks, 'y': np.arange(len(ks))})
+    # --- KOLMOGOROV-SMIRNOV OUTLIERS DETECTION --- #
+    # Transpose df again to have genes as columns and samples as rows
+    expression_df = expression_df.transpose()
 
-    q3 = ks_data['x'].quantile(0.75)
-    ks_limit = q3 + 1.5 * (ks_data['x'].quantile(0.75) - ks_data['x'].quantile(0.25))
+    # Function to calculate the Empirical Cumulative Distribution Function (ECDF) for one-dimensional data
+    def ecdf_1d(data):
+        n = len(data)  # Number of data points
+        x = np.sort(data)  # X-data for the ECDF
+        y = np.arange(1, n + 1) / n  # Y-data for the ECDF
+        return x, y
 
-    outliers['KS'] = {'limit': ks_limit, 'outliers': ks[ks > ks_limit]}
+    # Flatten the expression matrix to calculate the ECDF across all values
+    flat_values = expression_df.values.flatten()
+    x_ecdf, y_ecdf = ecdf_1d(flat_values)
 
-    print("Done!")
+    # Function to compute the KS statistic against the ECDF
+    def ks_statistic(sample):
+        cdf = lambda x: np.interp(x, x_ecdf, y_ecdf, left=0, right=1)
+        return kstest(sample, cdf).statistic
 
-    print("Running MAD Outliers Detection tests...")
-    rowExpression = expressionMatrix.mean(axis=1)
+    # Apply the KS test to each column (gene)
+    ks_results = expression_df.apply(ks_statistic)
 
-    outliersMA = []
-    for i in rowExpression.index:
-        exprMatrix = rowExpression.drop(i)
-        upperBound = exprMatrix.median() + 3 * mad(exprMatrix)
-        lowerBound = exprMatrix.median() - 3 * mad(exprMatrix)
+    # Identify outliers based on the KS test
+    q3 = ks_results.quantile(0.75)
+    iqr = ks_results.quantile(0.75) - ks_results.quantile(0.25)
+    ks_threshold = q3 + 1.5 * iqr
 
-        if rowExpression.loc[i] < lowerBound or rowExpression.loc[i] > upperBound:
-            outliersMA.append(i)
+    # Identify outliers based on the KS test
+    ks_outliers = ks_results[ks_results > ks_threshold].index.tolist()
+    outliers['KS'] = {'limit': ks_threshold, 'outliers': ks_outliers}
 
-        outliers['MAD'] = {'limit': "-", 'outliers': outliersMA}
+    # --- MAD OUTLIERS DETECTION --- #
+    # Calculate the median absolute deviation
+    """median_expression = np.median(expression_df, axis=0)
+    mad_scores = np.median(np.abs(expression_df - median_expression), axis=0)
 
-        print("Done!")
+    # Detect MAD-based outliers
+    mad_outliers = expression_df.columns[mad_scores > 3].tolist()
+    outliers['MAD'] = {'limit': 3, 'outliers': mad_outliers}"""
 
-        if not toRemoval:
-            return outliers
+    mad_outliers = []
+    row_expression = expression_df.iloc[:, 1:].mean()
+    for i in range(len(row_expression)):
+        expr_matrix = row_expression.drop(row_expression.index[i])
 
-        found_outliers = list(
-            set(outliers['Distance']['outliers']).intersection(outliers['KS']['outliers'], outliers['MAD']['outliers']))
-        expressionMatrix = expressionMatrix.drop(found_outliers, errors='ignore')
-        removed_outliers += found_outliers
+        upper_bound = expr_matrix.median() + 3 * median_abs_deviation(expr_matrix, scale=1)
+        lower_bound = expr_matrix.median() - 3 * median_abs_deviation(expr_matrix, scale=1)
 
-        return {'matrix': expressionMatrix, 'outliers': removed_outliers}
+        if row_expression.iloc[i] < lower_bound or row_expression.iloc[i] > upper_bound:
+            mad_outliers.append(row_expression.index[i])
+
+    outliers['MAD'] = {'limit': "-", 'outliers': mad_outliers}
+
+    # Get common outliers at least between two of three methods
+    common_outliers = set(distance_outliers) & set(ks_outliers) | set(distance_outliers) & set(mad_outliers) | set(
+        ks_outliers) & set(mad_outliers)
+
+    quality_matrix = expression_df.drop(columns=list(common_outliers))
+
+    return quality_matrix, list(common_outliers)
---
+++ tests/test_rna_seq_qa.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+
+import pandas as pd
+
+from knowseq.rna_seq_qa import rna_seq_qa
+
+
+class RnaSeqQaTest(unittest.TestCase):
+    def setUp(self):
+        qa_path = os.path.normpath(os.path.join("test_fixtures", "golden", "qa_matrix_breast.csv"))
+        self.golden_qa = pd.read_csv(qa_path, index_col=0, header=0)
+
+    def test_rna_seq_qa(self):
+        import csv
+        gene_expression_path = os.path.normpath(
+            os.path.join("test_fixtures", "golden", "gene_expression_matrix_breast.csv"))
+        gene_expression = pd.read_csv(gene_expression_path, header=0, index_col=0)
+
+        res_qa, _ = rna_seq_qa(gene_expression)
+        pd.testing.assert_frame_equal(res_qa, self.golden_qa, check_dtype=False, check_like=True,
+                                      check_exact=False, atol=0.1, rtol=0.1)
+
+    if __name__ == '__main__':
+        unittest.main()
